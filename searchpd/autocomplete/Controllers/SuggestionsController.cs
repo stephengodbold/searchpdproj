4218be615e9356046238ccf41056a3aacf090593
Matt Perdeck <matt_perdeck@yahoo.com>
2013-07-04 18:01:17 +1000

    converted suggestion web api to controler
    azure gives 404 errors on web api, but on controller.

---
+++ searchpd/autocomplete/Controllers/SuggestionsController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using searchpd.Models;
+using searchpd.Search;
+
+namespace autocomplete.Controllers
+{
+    public class SuggestionsController : Controller
+    {
+        private readonly ISearcher _searcher = null;
+
+        public SuggestionsController(ISearcher searcher)
+        {
+            _searcher = searcher;
+        }
+
+        //
+        // GET: /Suggestions/
+
+        public string Index(string q, string callback)
+        {
+            string subString = q;
+
+            IEnumerable<ISuggestion> suggestions = _searcher.FindSuggestionsBySubstring(subString);
+
+            var html = new StringBuilder();
+
+            foreach (ISuggestion suggestion in suggestions)
+            {
+                html.AppendFormat(@"<div class=""suggestion"">");
+                html.AppendFormat(suggestion.ToHtml(subString));
+                html.AppendFormat(@"</div>");
+            }
+
+            string finalHtml = html.ToString();
+            string jsonpResponse = string.Format("{0}(\'{1}\')", callback, HttpUtility.JavaScriptStringEncode(finalHtml));
+            return jsonpResponse;
+        }
+
+    }
+}
